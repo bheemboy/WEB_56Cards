697ca2f8f2746ac382b7ae390bbd914f3c874749
Sunil Rehman <sunil.rehman@gmail.com>
2025-02-22 15:17:19 -0800

    updated to svelte 5

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,67 +1,59 @@
 <script lang="ts">
-    export let card: string;
-    export let rotation: number = 0;
-    export let translation: number = 0;
-    export let scale: number = 15;
+    const { card, rotation = 0, translation = 0, scale = 15 } = $props();
 
     const suits = new Map<string, number>([['C', 0],['D', 1],['H', 2],['S', 3]]);
-    const ranks = new Map<string, number>([['0', 0],['A', 1],['2', 2],['3', 3],['4', 4],['5', 5],['6', 6],['7', 7],['8', 8],['9', 9],['10', 10],['J', 11],['Q', 12],['K', 13]]);
+    const ranks = new Map<string, number>([['0', 0],['A', 1],['2', 2],['3', 3],['4', 4],
+        ['5', 5],['6', 6],['7', 7],['8', 8],['9', 9],['10', 10],['J', 11],['Q', 12],['K', 13]]);
 
-    // get the card code from the cards array
     const suit = suits.get(card[0]);
     const rank = ranks.get(card.slice(1));
-    const isValidCard = suit !== undefined && rank !== undefined
+    const isValidCard = suit !== undefined && rank !== undefined;
+
     if (!isValidCard) {
         console.error(`Invalid card ${card}`);
     }
 
-    // calculate card height and width based on the window height
     const pngCardHeight = 94;
     const pngCardWidth = 69;
     const aspectRatio = pngCardWidth / pngCardHeight;
 
-    let windowHeight:number; // window height will initially be undefined, but will be updated by the time the window is displayed
+    let windowHeight = $state<number>();
+    const card_height = $derived(Math.max(pngCardHeight, ((windowHeight ?? 0) * scale / 100)));
+    const card_width = $derived(card_height * aspectRatio);
 
-    let card_height = pngCardHeight;
-    $: if (windowHeight) {
-        let calculatedHeight = (windowHeight * scale) / 100;
-        if (calculatedHeight >= pngCardHeight) {
-            card_height = calculatedHeight;
-        }
-    }
-    $: card_width = card_height * aspectRatio;
-    
-    // calculate the size and position for the background image based on the card height and width
-    $: background_scale_ratio = card_height / (1.0 * pngCardHeight);
-    $: background_size = (966 * background_scale_ratio).toString() + "px " + (376 * background_scale_ratio).toString() + "px";
-    $: bgPositionY = isValidCard ? -1 * suit * pngCardHeight * background_scale_ratio  : 0;
-    $: bgPositionX = isValidCard ? -1 * rank * pngCardWidth * background_scale_ratio : 0;
-    $: background_position = bgPositionX.toString() + "px " + bgPositionY.toString() + "px";
-
+    const background_scale_ratio = $derived(card_height / pngCardHeight);
+    const background_size = $derived(`${966 * background_scale_ratio}px ${376 * background_scale_ratio}px`);
+    const bgPositionY = $derived(isValidCard ? -1 * suit * pngCardHeight * background_scale_ratio : 0);
+    const bgPositionX = $derived(isValidCard ? -1 * rank * pngCardWidth * background_scale_ratio : 0);
+    const background_position = $derived(`${bgPositionX}px ${bgPositionY}px`);
 </script>
 
 <svelte:window bind:innerHeight={windowHeight} />
 
 {#if isValidCard}
-    <button class="card"
-        on:click={() => alert(card)}
-        aria-label="Card"
-        style=
-            "--card-height: {card_height}px;  --card-width: {card_width}px;
-            --background-size: {background_size};  --background-position: {background_position}; 
-            --rotation: rotate({rotation}deg); --translation: {translation}px;" >
-    </button>
+    <div 
+        class="card"
+        role="button"
+        tabindex="0"
+        onclick={() => alert(card)}
+        onkeydown={() => {}}
+        aria-label={`${card} playing card`}
+        style="--card-height: {card_height}px;
+               --card-width: {card_width}px;
+               --background-size: {background_size};
+               --background-position: {background_position};
+               --rotation: rotate({rotation}deg);
+               --translation: {translation}px;">
+    </div>
 {:else}
-    <div class="error">Invalid card {card}</div>
+    <div class="error" role="alert">Invalid card {card}</div>
 {/if}
-
 
 <style>
     .card {
         position: absolute;
         border: none;
         outline: none;
-        border-radius: 12px;
         padding: 0;
         top: 9%;
         --element-width: 0px;
@@ -74,6 +66,7 @@
         transform-origin: 30% 100%;
         transform: var(--rotation);
         translate: var(--translation);
+        transition: top 0.1s ease-out;
     }
     .card:hover {
         top: 7%;
