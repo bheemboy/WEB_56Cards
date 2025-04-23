8da3095503f41334033b0560d0e30a9a18f64ecc
Sunil Rehman <sunil.rehman@gmail.com>
2025-04-22 17:04:25 -0700

    Charis layout defined for portrait mobile.

--- src/lib/Chairs.svelte
+++ src/lib/Chairs.svelte
@@ -58,79 +58,21 @@
 
 </script>
 
-<div class={`chair-container  p${game.tableInfo.maxPlayers} `}>
-  {#each game.chairs.getAllChairs() as chair}
-    <!-- {#if chair.Position !== game.currentPlayer.playerPosition} -->
-      <div class={`chair ${getPositionClass(chair)} ${getTeamClass(chair)}`}>
-        {chair.Occupant?.Name ?? ""}
-      </div>
-    <!-- {/if} -->
-  {/each}
-</div>
+{#each game.chairs.getAllChairs() as chair}
+  <!-- {#if chair.Position !== game.currentPlayer.playerPosition} -->
+    <div class={`chair ${getPositionClass(chair)} ${getTeamClass(chair)}`}>
+      {chair.Occupant?.Name ?? ""}
+    </div>
+  <!-- {/if} -->
+{/each}
 
 <style>
-  .chair-container {
-    position: absolute;
-    display: grid;
-    width: 100cqw;
-    height: 95cqh;
-    top: 5cqh;
-    left: 0cqw;
-    /* background-color: rgb(22, 256, 22, 0.5); */
-  }
-
-  .chair-container.p4 {
-    grid-template-areas:
-      ". T ."
-      "L . R"
-      ". B .";
-  }
-
-  .chair-container.p6 {
-    grid-template-columns: 1fr 3fr 4fr 3fr 1fr;
-    grid-template-rows: 1fr 2fr 4fr 2fr 4fr 2fr 1fr;
-    grid-template-areas:
-      " .   .   T   .   ."
-      " .   .   .   .   ."
-      "L_T  .   .   .  R_T"
-      " .   .   .   .   ."
-      "L_B  .   .   .  R_B"
-      " .   .   .   .   ."
-      " .   .   B   .   .";
-  }
-
-  .chair-container.p8 {
-    grid-template-areas:
-      ".   .   .   T   .   .   ."
-      ".  L_T  .   T   .  R_T  ."
-      ".  L_T  .   .   .  R_T  ."
-      "L   .   .   .   .   .   R"
-      "L   .   .   .   .   .   R"
-      ".  L_B  .   .   .  R_B  ."
-      ".  L_B  .   B   .  R_B  ."
-      ".   .   .   B   .   .   .";
-  }
-
-  @media (width > 450px) {
-    .chair-container {
-      /* width: 50cqw; */
-      gap: min(1cqw, 1cqh);
-    }
-  }
-
-  @media (width > 780px) {
-    .chair-container {
-      /* width: 40cqw; */
-      gap: min(2cqw, 2cqh);
-    }
-  }
-
   .chair {
-    position: relative;
+    position: absolute;
     writing-mode: vertical-rl;
     text-orientation: mixed;
     font-size: 1.5rem;
-    background-color: rgba(0, 0, 255, 0.4);
+    background-color: rgba(0, 0, 255, 0.3);
     border: 1px solid rgba(0, 0, 255, 0.5);
     width: 2rem;
     height: 8rem;
@@ -147,43 +89,49 @@
   }
 
   .B {
-    grid-area: B;
     writing-mode: horizontal-tb;
+    width: 8rem;
     height: 2rem;
-    width: auto;
     z-index: 1;
-    /* bottom: 0px; */
+    bottom: 0cqh;
   }
 
   .R_B {
-    grid-area: R_B;
+    right: 0cqw;
+    top: 57cqh; 
   }
 
   .R {
-    grid-area: R;
+    right: 0cqw;
+    top: 37cqh;
   }
 
   .R_T {
-    grid-area: R_T;
+    right: 0cqw;
+    top: 17cqh; 
   }
 
   .T {
     grid-area: T;
     writing-mode: horizontal-tb;
+    width: 8rem;
     height: 2rem;
-    width: auto;
     z-index: 1;
+    top: min(50px, min(13cqw, 13cqh));
   }
 
   .L_T {
-    grid-area: L_T;
+    left: 0cqw;
+    top: 17cqh; 
   }
 
   .L {
-    grid-area: L;
+    left: 0cqw;
+    top: 37cqh; 
   }
 
   .L_B {
-    grid-area: L_B;
+    left: 0cqw;
+    top: 57cqh; 
   }
 </style>
